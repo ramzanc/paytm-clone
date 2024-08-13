2f439f3b5e85ca689f539d359b0d516762cf909b
Ramzan <ramzan.crescent25@gmail.com>
2024-08-13 12:48:53 +0530

    Add user model

---
+++ backend/db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+async function connect() {
+    await mongoose.connect("mongodb://localhost:27017/paytm")
+}
+
+connect()
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+    User
+}
+
